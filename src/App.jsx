f48fdf4a9922d9c2bcd5430c91061cc634162028
AngeloC <geloycabangal10@gmail.com>
2025-08-22 18:09:52 +0800

    Initial commit with all CP2-WhiskeyWatch-Clean files

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import Home from './pages/Home'
 import AboutUs from './pages/AboutUs'
 import CatCareGuides from './pages/CatCareGuides'
 import ContactUs from './pages/ContactUs'
-import Login from './pages/Login'
+import Login from './pages/LoginAndSignUp/Login'
 import CatCommunityNews from './pages/Home/CatCommunityNews'
 import Donate from './pages/Donate'
 import CatAdoption from './pages/CatAdoption'
 import Feeding from './pages/Feeding'
 import CommunityGuide from './pages/CommunityGuide'
+import AdminLogin from './pages/LoginAndSignUp/AdminLogin'
+import SignUp from './pages/LoginAndSignUp/SignUp'
 
 
 
@@ -20,12 +22,15 @@ const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
         <Route path="home" element={<Home />} />
         <Route path="catcommunitynews" element={<CatCommunityNews />} />
         <Route path="aboutus" element={<AboutUs />} />
         <Route path="catcareguides" element={<CatCareGuides />} />
         <Route path="contactus" element={<ContactUs />} />
         <Route path="login" element={<Login />} />
+        <Route path="adminlogin" element={<AdminLogin />}/> 
+        <Route path="signup" element={<SignUp />} />
         <Route path="donate" element={<Donate />} />
         <Route path="catadoption" element={<CatAdoption />} />
         <Route path="feeding" element={<Feeding />} />
